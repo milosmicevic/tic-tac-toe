7823435d1daaf7531e9d63eea6d59c3d4f56fbba
Milos Micevic <72022797+milosmicevic@users.noreply.github.com>
2021-11-02 13:51:06 +0100

    Added game functionality

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-// Javascript
+const boardContainer = document.querySelector("#board-container");
+const fields = document.querySelectorAll(".field");
+
+const board = [];
+const eksIndexes = [];
+const oksIndexes = [];
+
+let turnCounter = 1;
+
+boardContainer.addEventListener("click", (e) => {
+  if (e.target.innerHTML === "") {
+    const fieldIndex = e.target.id;
+    e.target.appendChild(
+      turnCounter % 2 !== 0 ? eksSign(fieldIndex) : oksSign(fieldIndex)
+    );
+    turnCounter++;
+    checkWinner();
+  }
+
+  if (turnCounter > 9) {
+    alert("It's Draw!");
+  }
+});
+
+function eksSign(fieldIndex) {
+  const eks = document.createElement("span");
+  eks.classList.add("eks");
+
+  board[fieldIndex] = "x";
+  eksIndexes.push(fieldIndex);
+
+  return eks;
+}
+
+function oksSign(fieldIndex) {
+  const oks = document.createElement("span");
+  oks.classList.add("oks");
+
+  board[fieldIndex] = "o";
+  oksIndexes.push(fieldIndex);
+
+  return oks;
+}
+
+function checkWinner() {
+  const winCombos = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"],
+    ["0", "3", "6"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["0", "4", "8"],
+    ["2", "4", "6"],
+  ];
+
+  winCombos.forEach((combo) => {
+    if (combo.every((index) => eksIndexes.includes(index))) {
+      alert("X has Won!");
+    } else if (combo.every((index) => oksIndexes.includes(index))) {
+      alert("O has Won!");
+    }
+  });
+}
